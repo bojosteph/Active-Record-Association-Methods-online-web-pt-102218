8b0fdb4fd2ba6eb2220b6ddbf857e186cbe653fc
Roberto Samia <bojosteph@gmail.com>
2019-01-24 02:26:02 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,7 +9,7 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    self.songs
+    self.artists.size
   end
 
   def all_artist_names
